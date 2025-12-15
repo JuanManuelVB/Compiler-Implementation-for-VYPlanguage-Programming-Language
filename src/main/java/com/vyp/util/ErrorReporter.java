a1c91d86d04c763067feec51b9a8e5904e69b6f8
Juan Manuel <juanmanuelvargasbellido@gmail.com>
2025-12-15 13:48:49 +0100

    Add initial implementation of VYP language compiler
    - Created ExitCodes class to define exit codes for the compiler.
    - Added source files for the VYP language compiler, including AST, semantic analysis, and code generation components.
    - Included generated ANTLR runtime files necessary for parsing and tree manipulation.
    - Established a structure for error reporting with various exception classes for handling lexical, syntax, and semantic errors.

--- src/main/java/com/vyp/util/ErrorReporter.java
+++ src/main/java/com/vyp/util/ErrorReporter.java
@@ -1,32 +1,68 @@
 package com.vyp.util;
 
 import com.vyp.frontend.ast.SourceLocation;
+import com.vyp.ExitCodes;
 
 public class ErrorReporter {
 
-    public class CompilerException extends RuntimeException {
+    /** Generic compiler exception carrying an exit code and optional source location. */
+    public static class CompilerException extends RuntimeException {
         private final SourceLocation loc;
+        private final int exitCode;
 
-        public CompilerException(String msg, SourceLocation location) {
+        public CompilerException(String msg, SourceLocation location, int exitCode) {
             super(msg);
             this.loc = location;
+            this.exitCode = exitCode;
         }
 
-        public SourceLocation getLocation() {
-            return loc;
-        }
+        public SourceLocation getLocation() { return loc; }
+        public int getExitCode() { return exitCode; }
     }
 
-    class DuplicateSymbolException extends CompilerException {
-
-        public DuplicateSymbolException(String msg, SourceLocation location) {
-            super(msg, location);
-
-        }
+    public static class LexicalException extends CompilerException {
+        public LexicalException(String msg, SourceLocation loc) { super(msg, loc, ExitCodes.LEXICAL_ERROR); }
     }
 
-    public void error(SourceLocation location, String string) {
-        throw new CompilerException("Error at " + location + ": " + string, location);
+    public static class SyntaxException extends CompilerException {
+        public SyntaxException(String msg, SourceLocation loc) { super(msg, loc, ExitCodes.SYNTAX_ERROR); }
+    }
+
+    public static class SemanticTypeException extends CompilerException {
+        public SemanticTypeException(String msg, SourceLocation loc) { super(msg, loc, ExitCodes.SEMANTIC_TYPE_ERROR); }
+    }
+
+    public static class SemanticException extends CompilerException {
+        public SemanticException(String msg, SourceLocation loc) { super(msg, loc, ExitCodes.SEMANTIC_OTHER_ERROR); }
+    }
+
+    public static class CodegenException extends CompilerException {
+        public CodegenException(String msg, SourceLocation loc) { super(msg, loc, ExitCodes.CODEGEN_ERROR); }
+    }
+
+    public static class InternalException extends CompilerException {
+        public InternalException(String msg, SourceLocation loc) { super(msg, loc, ExitCodes.INTERNAL_ERROR); }
+    }
+
+    // Convenience methods to throw typed exceptions from analyses
+    public void lexical(SourceLocation loc, String msg) { throw new LexicalException(format(msg, loc, ExitCodes.LEXICAL_ERROR), loc); }
+    public void syntax(SourceLocation loc, String msg) { throw new SyntaxException(format(msg, loc, ExitCodes.SYNTAX_ERROR), loc); }
+    public void semanticType(SourceLocation loc, String msg) { throw new SemanticTypeException(format(msg, loc, ExitCodes.SEMANTIC_TYPE_ERROR), loc); }
+    public void semantic(SourceLocation loc, String msg) { throw new SemanticException(format(msg, loc, ExitCodes.SEMANTIC_OTHER_ERROR), loc); }
+    public void codegen(SourceLocation loc, String msg) { throw new CodegenException(format(msg, loc, ExitCodes.CODEGEN_ERROR), loc); }
+    public void internal(SourceLocation loc, String msg) { throw new InternalException(format(msg, loc, ExitCodes.INTERNAL_ERROR), loc); }
+
+    // Backwards-compatible API: generic error -> semantic other
+    public void error(SourceLocation location, String string) { semantic(location, string); }
+
+    private String format(String msg, SourceLocation loc) {
+        return format(msg, loc, -1);
+    }
+
+    private String format(String msg, SourceLocation loc, int exitCode) {
+        String locPrefix = (loc != null ? ("Error at " + loc + ": ") : "Error: ");
+        String codeSuffix = exitCode >= 0 ? (" [code=" + exitCode + "]") : "";
+        return locPrefix + msg + codeSuffix;
     }
 
 }
