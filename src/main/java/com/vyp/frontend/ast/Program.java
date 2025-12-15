66f88d5c49ebbb35d5d81e2c9b0cb245a867d09d
Juan Manuel <juanmanuelvargasbellido@gmail.com>
2025-12-15 17:12:14 +0100

    Refactor TestASTAndSymbols and TestCodegen for improved readability; add global variable support in Program class

--- src/main/java/com/vyp/frontend/ast/Program.java
+++ src/main/java/com/vyp/frontend/ast/Program.java
@@ -6,15 +6,19 @@ import java.util.List;
 public class Program extends ASTNode {
 
     private List<FunctionDecl> functions;
+    private List<VarDecl> globals;   // new
 
-    public Program(List<FunctionDecl> functions, SourceLocation location) {
+    public Program(List<VarDecl> globals, List<FunctionDecl> functions, SourceLocation location) {
         super(location);
+        this.globals = globals;
         this.functions = functions;
     }
 
     public List<FunctionDecl> getFunctions() {
         return functions;
     }
+
+    public List<VarDecl> getGlobals() { return globals; }
 
     @Override
     public <T> T accept(ASTVisitor<T> visitor) {
